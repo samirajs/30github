bc09758cff5d574cc392a424ede1dac47df29ee5
samira <miralivev7645@gmail.com>
2025-02-11 19:47:47 +0600

    Add files via upload

---
+++ Variables.java/code.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int age = 25;
+        double height = 1.75;
+        String name = "Алексей";
+        boolean isStudent = true;
+
+        
+        System.out.println("Имя: " + name); 
+        System.out.println("Возраст: " + age); 
+        System.out.println("Рост: " + height + " метров");
+        System.out.println("Является студентом? " + isStudent); 
+    }
+}
