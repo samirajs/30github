b0da9a2d0d5f19dfcfbc9d75e0e9d5692cd07386
samira <miralivev7645@gmail.com>
2025-02-18 19:37:46 +0600

    Add files via upload

---
+++ DataTypes.java/index.java
@@ -0,0 +1,19 @@
+public class Example {
+
+    public static void main(String[] args) {
+    
+    int id = 101;
+    
+    String name = "Alice";
+    
+    Double gpa = 3.75;
+    
+    System.out.println("ID: " + id);
+    
+    System.out.println("Name: " + name);
+    
+    System.out.println("GPA: " + gpa);
+    
+    }
+    
+    }
