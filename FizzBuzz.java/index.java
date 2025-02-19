9d99a2f8cffc06c82111a768c7d27208ac1a7c30
samira <miralivev7645@gmail.com>
2025-02-19 13:16:04 +0600

    Add files via upload

---
+++ FizzBuzz.java/index.java
@@ -0,0 +1,13 @@
+class Solution {
+    public:
+        vector<string> fizzBuzz(int n) {
+            n = 3
+            ["1","2","Fizz"]
+    
+            n = 5 
+            ["1","2","Fizz","4","Buzz"]
+            
+            n = 15
+            ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+        }
+    };
