abd7c0df979e06eeefaedbd2875a03269ae6fa74
samira <miralivev7645@gmail.com>
2025-02-23 21:07:16 +0600

    Add files via upload

---
+++ MergeSortedArray.java/insex.java
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+        void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+          nums1=[1,2,3,0,0,0];
+          m=3;
+          nums2=[2,5,6];
+          n=[3];
+    
+          nums1=[1];
+          m=1;
+          nums2=[];
+          n=0;
+    
+          nums1=[0];
+          m=0
+          nums2=[1]
+          n=1
+        }
+    };
